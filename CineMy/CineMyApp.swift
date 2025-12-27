6773e93b3bf5fd88ffb23afe32161b753ff24b21
Prachi Sawan <prachianand7@gmail.com>
2025-12-27 12:40:22 +0530

    Initial Commit

---
+++ CineMy/CineMyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMyApp.swift
+//  CineMy
+//
+//  Created by Prachi Sawan on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CineMyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineMyTests/CineMyTests.swift
@@ -0,0 +1,16 @@
+//
+//  CineMyTests.swift
+//  CineMyTests
+//
+//  Created by Prachi Sawan on 27/12/25.
+//
+
+import Testing
+
+struct CineMyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
